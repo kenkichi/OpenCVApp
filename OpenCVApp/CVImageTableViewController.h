429b7fa129418f43b617fbcfafc2c9ec025f7f39
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2013-03-18 15:07:47 +0900

    撮影した写真をAppHome/Documentsへ保存するようにした

---
+++ OpenCVApp/CVImageTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  CVImageTableViewController.h
+//  OpenCVApp
+//
+//  Created by thata on 2013/03/18.
+//  Copyright (c) 2013年 chikuwaprog.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CVImageTableViewController : UITableViewController
+- (IBAction)takePhoto:(id)sender;
+
+@end
---
+++ OpenCVApp/CVImageTableViewController.m
@@ -0,0 +1,158 @@
+//
+//  CVImageTableViewController.m
+//  OpenCVApp
+//
+//  Created by thata on 2013/03/18.
+//  Copyright (c) 2013年 chikuwaprog.com. All rights reserved.
+//
+
+#import "CVImageTableViewController.h"
+
+@interface CVImageTableViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@end
+
+@implementation CVImageTableViewController
+
+- (IBAction)takePhoto:(id)sender {
+    // カメラを起動
+    UIImagePickerController *imagePicker;
+    imagePicker = [[UIImagePickerController alloc] init];
+    imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    imagePicker.delegate = self;
+    [self presentViewController:imagePicker
+                       animated:YES
+                     completion:nil];
+}
+
+#pragma mark - UIImagePickerControllerDelegate
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    // 撮影した画像を取得し、
+    UIImage *image = [info objectForKey:UIImagePickerControllerOriginalImage];
+    // カメラロールに保存する
+    NSData *data = UIImageJPEGRepresentation(image, 1.0);
+    NSUUID *uuid = [NSUUID UUID];
+    NSString *fileName = [NSString stringWithFormat:@"%@.jpg", [uuid UUIDString]];
+    NSFileManager *fm = [NSFileManager defaultManager];
+    NSURL *documentRoot = [[fm URLsForDirectory:NSDocumentDirectory
+                             inDomains:NSUserDomainMask] objectAtIndex:0];
+    NSURL *url = [documentRoot URLByAppendingPathComponent:fileName];
+    [data writeToURL:url atomically:YES];
+
+    // カメラを閉じる
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma mark - UIViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     */
+}
+
+@end
